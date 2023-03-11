ca9924cb71de67a959ad64590277e979e85fda60
soojlee0106 <soojlee0106@naver.com>
2023-03-11 21:25:51 +0900

    Finish initial http

--- frontend/server.js
+++ frontend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
