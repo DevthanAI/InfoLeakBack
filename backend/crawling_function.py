98c85db1a0b561e85f219d1425c6dc34151f49d2
Soo <soojlee0106@naver.com>
2023-04-05 18:45:13 +0900

    Optimized version

--- backend/crawling_function.py
+++ backend/crawling_function.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import visibility_of_element_located
+from selenium.common.exceptions import TimeoutException
 
 
 def get_driver():
@@ -11,34 +14,32 @@ def get_driver():
 
 options = Options()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
-options.add_argument("window-size=10000,10000")
+options.add_argument("window-size=50,100")
 options.add_argument("lang=en-GB")
 options.add_argument('--disable-gpu')
 options.add_argument("no-sandbox")
-# options.add_argument('--headless')
 
 
 def crawl_driver(keywords=[]):
-    driver = get_driver()
-    driver.get('https://www.google.com/')
-    search_box = driver.find_element('name', 'q')
-    info_list = []
+    with get_driver() as driver:
+        driver.get('https://www.google.com/')
+        search_box = driver.find_element(By.CSS_SELECTOR, 'input[name="q"]')
+        info_list = []
 
-    for i in range(len(keywords)):
-        search_box.send_keys(keywords[i])
-        search_box.submit()
-        driver.implicitly_wait(10)
-
-        span_elements = driver.find_elements(By.ID, 'rso')
-
-        for element in span_elements:
-            info_list.append(element.text)
-
-        driver.back()
+        for keyword in keywords:
+            search_box.clear()
+            search_box.send_keys(keyword)
+            search_box.submit()
+            try:
+                WebDriverWait(driver, 2).until(
+                    visibility_of_element_located((By.ID, 'rso')))
+            except TimeoutException:
+                info_list.append('no results found')
+                driver.back()
+                continue
+            span_elements = driver.find_elements(By.ID, 'rso')
+            info_list += [element.text for element in span_elements]
+            driver.back()
 
     print(info_list)
     return info_list
-
-
-# crawl_driver(keywords=["soolee0701", "soojlee0106",
-#              "ofdetectivesandcats", "010-8839-2919"])
